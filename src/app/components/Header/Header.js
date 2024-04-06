ab07c6f519ab99614eb7a4bf047e19c3d91501eb
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2024-04-06 11:10:26 +0100

    :sparkles: added SnipCart inital functionality

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
-
+import Script from "next/script";
 import styles from "./Header.module.scss";
 
 export default function Header() {
   return (
-    <div className={styles["FSC__header"]}>
-      <h1 className={styles["FSC__header__title"]}>Skill Tree Collectables</h1>
-      <FaShoppingCart />
+    <div>
+      <div className={styles["FSC__header"]}>
+        <h1 className={styles["FSC__header__title"]}>
+          Skill Tree Collectables
+        </h1>
+
+        <button className="snipcart-checkout  FSC__Buttons__primary">
+          <FaShoppingCart />
+          <span class="snipcart-total-price"></span>
+        </button>
+      </div>
+
+      {/* //snipcart */}
     </div>
   );
 }
